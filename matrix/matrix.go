d6a1281fa20102d63bc07a7f8bd253c129f131b7
Tom Ratford <tomratford@gmail.com>
2023-12-18 21:05:13 +0000

    feat(green): transpose non-square matrices

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,9 +1,20 @@
 package matrix
 
+import "reflect"
+
 type matrix = [][]float64
 
 func Transpose(m matrix) matrix {
-	m2 := make(matrix, len(m), len(m))
+	// Empty matrix
+	if reflect.DeepEqual(m, matrix{}) {
+		return m
+	}
+
+	nrow := len(m)
+	ncol := len(m[0])
+
+	m2 := make(matrix, ncol, max(ncol, nrow))
+
 	for _, v1 := range m {
 		for j, v2 := range v1 {
 			m2[j] = append(m2[j], v2)
