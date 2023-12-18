72078c1ff479056fa5ab5830688cd6989ea83bbf
Tom Ratford <tomratford@gmail.com>
2023-12-18 20:34:29 +0000

    feat: Read in CSV and basic CLI

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+func usage() {
+	fmt.Printf("usage: ols <input.csv> <response> ~ [exploratory]\n")
+}
 
 func main() {
-	fmt.Printf("Hello World!\n")
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	records, err := ReadFromCSV(os.Args[1])
+
+	// Check if the error is that the file isn't real
+	if os.IsNotExist(err) {
+		usage()
+		fmt.Printf("error: file '%s' does not exist\n", os.Args[1])
+	} else if err != nil {
+		usage()
+		fmt.Println(err)
+	}
+
+	fmt.Println(records)
 }
+
+func ReadFromCSV(filepath string) ([][]string, error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return [][]string{}, err
+	}
+
+	r := csv.NewReader(file)
+	return r.ReadAll()
+}
+
+/*
+   Essentially I now need a matrix library.
+   Because however I swing it I'm gonna be inverting some kinda matrix
+*/
