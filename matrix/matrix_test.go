84e0209f609d081df9afe6db15e165db85910ea0
Tom Ratford <tomratford@gmail.com>
2023-12-18 21:04:51 +0000

    feat(red): transpose non square matrices

--- matrix/matrix_test.go
+++ matrix/matrix_test.go
@@ -36,4 +36,39 @@ func TestTranspose(t *testing.T) {
 			t.Errorf("expected to be the same, got %v, want %v", got, want)
 		}
 	})
+
+	t.Run("transpose of a tall one by four", func(t *testing.T) {
+		m := matrix{
+			{1.4},
+			{3.2},
+			{2.9},
+			{0.3},
+		}
+		got := Transpose(m)
+		want := matrix{
+			{1.4,3.2,2.9,0.3},
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("expected to be the same, got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("transpose of a wide four by two", func(t *testing.T) {
+		m := matrix{
+			{1.4,3.2,2.9,0.3},
+			{4.4,2.0,9.3,3.8},
+		}
+		got := Transpose(m)
+		want := matrix{
+			{1.4, 4.4},
+			{3.2, 2.0},
+			{2.9, 9.3},
+			{0.3, 3.8},
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("expected to be the same, got %v, want %v", got, want)
+		}
+	})
 }
