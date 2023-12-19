cf55d2773a35cc293ec762e8090c6bafdbcc38cf
Tom Ratford <tomratford@gmail.com>
2023-12-19 12:23:42 +0000

    refactor: switch to table tests

--- matrix/matrix_test.go
+++ matrix/matrix_test.go
@@ -6,69 +6,70 @@ import (
 )
 
 func TestTranspose(t *testing.T) {
-	t.Run("transpose of the identity is the identity", func(t *testing.T) {
-		m := matrix{
-			{1,0,0},
+	type TestCase struct {
+		desc string
+		input, want matrix
+	}
+
+	test_cases := []TestCase{
+		TestCase{
+			desc: "transpose of the identity is the identity",
+			input: matrix{
+				{1,0,0},
 				{0,1,0},
 				{0,0,1},
+			},
+			want: matrix{
+				{1,0,0},
+				{0,1,0},
+				{0,0,1},
+			},
+		},
+		TestCase{
+			desc: "transpose of a simple 2x2 matrix",
+			input: matrix{
+				{2,3},
+				{8,1},
+			},
+			want: matrix{
+				{2,8},
+				{3,1},
+			},
+		},
+		TestCase{
+			desc: "transpose of a tall 4x1 matrix",
+			input: matrix{
+				{1.4},
+				{3.2},
+				{2.9},
+				{0.3},
+			},
+			want: matrix{
+				{1.4,3.2,2.9,0.3},
+			},
+		},
+		TestCase{
+			desc: "transpose of a wide two by four matrix",
+			input: matrix{
+				{1.4,3.2,2.9,0.3},
+				{4.4,2.0,9.3,3.8},
+			},
+			want: matrix{
+				{1.4, 4.4},
+				{3.2, 2.0},
+				{2.9, 9.3},
+				{0.3, 3.8},
+			},
+		},
+	}
+
+	for _, test_case := range test_cases {
+		t.Run(test_case.desc, func(t *testing.T) {
+			got := Transpose(test_case.input)
+
+			if !reflect.DeepEqual(got, test_case.want) {
+				t.Errorf("expected to be the same, got %v, want %v", got, test_case.want)
 			}
-
-		got := Transpose(m)
-		want := m
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("expected transpose of the identity to be the same, got %v", got)
-		}
-	})
-
-	t.Run("transpose of a simple two by two", func(t *testing.T) {
-		m := matrix{
-			{2,3},
-			{8,1},
-		}
-		got := Transpose(m)
-		want := matrix{
-			{2,8},
-			{3,1},
-		}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("expected to be the same, got %v, want %v", got, want)
-		}
-	})
-
-	t.Run("transpose of a tall one by four", func(t *testing.T) {
-		m := matrix{
-			{1.4},
-			{3.2},
-			{2.9},
-			{0.3},
-		}
-		got := Transpose(m)
-		want := matrix{
-			{1.4,3.2,2.9,0.3},
-		}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("expected to be the same, got %v, want %v", got, want)
-		}
-	})
-
-	t.Run("transpose of a wide four by two", func(t *testing.T) {
-		m := matrix{
-			{1.4,3.2,2.9,0.3},
-			{4.4,2.0,9.3,3.8},
-		}
-		got := Transpose(m)
-		want := matrix{
-			{1.4, 4.4},
-			{3.2, 2.0},
-			{2.9, 9.3},
-			{0.3, 3.8},
-		}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("expected to be the same, got %v, want %v", got, want)
-		}
-	})
+		})
+	}
 }
