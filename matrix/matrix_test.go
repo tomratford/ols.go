f599bf3d0319696c1689bc86d05d2d2560eaaf70
Tom Ratford <tomratford@gmail.com>
2023-12-22 21:20:12 +0000

    refactor: remove TestZero and add helper function
    We cannot TestZero as this is now our initaliser, we instead just
    will hope it works by extensive use.

--- matrix/matrix_test.go
+++ matrix/matrix_test.go
@@ -5,40 +5,19 @@ import (
 	"testing"
 )
 
-func TestZero(t *testing.T) {
-	type TestCase struct {
-		desc string
-		p, q int
-		want matrix
-	}
-	test_cases := []TestCase{
-		{
-			desc: "2 x 2 zero",
-			p:    2,
-			q:    2,
-			want: matrix{
-				{0, 0},
-				{0, 0},
-			},
-		},
-		{
-			desc: "1 x 3 zero",
-			p:    1,
-			q:    3,
-			want: matrix{
-				{0, 0, 0},
-			},
-		},
-	}
+// Helper function to convert slice of slices to a matrix object
+func fromSliceOfSlices(x [][]float64) matrix {
+	p := len(x)
+	q := len(x[0])
 
-	for _, test_case := range test_cases {
-		t.Run(test_case.desc, func(t *testing.T) {
-			got := Zero(test_case.p, test_case.q)
-			if !reflect.DeepEqual(got, test_case.want) {
-				t.Errorf("expected to be equal, got %v, want %v", got, test_case.want)
-			}
-		})
+	z := Zero(p, q)
+
+	for i := 0; i < p; i++ {
+		for j := 0; j < q; j++ {
+			z.Set(i,j,x[i][j])
+		}
 	}
+	return z
 }
 
 func TestTranspose(t *testing.T) {
@@ -50,52 +29,52 @@ func TestTranspose(t *testing.T) {
 	test_cases := []TestCase{
 		{
 			desc: "transpose of the identity is the identity",
-			input: matrix{
+			input: fromSliceOfSlices([][]float64{
 				{1, 0, 0},
 				{0, 1, 0},
 				{0, 0, 1},
-			},
-			want: matrix{
+			}),
+			want: fromSliceOfSlices([][]float64{
 				{1, 0, 0},
 				{0, 1, 0},
 				{0, 0, 1},
-			},
+			}),
 		},
 		{
 			desc: "transpose of a simple 2x2 matrix",
-			input: matrix{
+			input: fromSliceOfSlices([][]float64{
 				{2, 3},
 				{8, 1},
-			},
-			want: matrix{
+			}),
+			want: fromSliceOfSlices([][]float64{
 				{2, 8},
 				{3, 1},
-			},
+			}),
 		},
 		{
 			desc: "transpose of a tall 4x1 matrix",
-			input: matrix{
+			input: fromSliceOfSlices([][]float64{
 				{1.4},
 				{3.2},
 				{2.9},
 				{0.3},
-			},
-			want: matrix{
+			}),
+			want: fromSliceOfSlices([][]float64{
 				{1.4, 3.2, 2.9, 0.3},
-			},
+			}),
 		},
 		{
 			desc: "transpose of a wide two by four matrix",
-			input: matrix{
+			input: fromSliceOfSlices([][]float64{
 				{1.4, 3.2, 2.9, 0.3},
 				{4.4, 2.0, 9.3, 3.8},
-			},
-			want: matrix{
+			}),
+			want: fromSliceOfSlices([][]float64{
 				{1.4, 4.4},
 				{3.2, 2.0},
 				{2.9, 9.3},
 				{0.3, 3.8},
-			},
+			}),
 		},
 	}
 
@@ -117,15 +96,15 @@ func TestMultiply(t *testing.T) {
 	}
 
 	t.Run("fail on invalid multiplication (pxn * nxq)", func(t *testing.T) {
-		m1 := matrix{
+		m1 := fromSliceOfSlices([][]float64{
 			{2, 3},
 			{2, 3},
-		}
-		m2 := matrix{
+		})
+		m2 := fromSliceOfSlices([][]float64{
 			{1},
 			{2},
 			{3},
-		}
+		})
 		_, err := Multiply(m1, m2)
 		if err == nil {
 			t.Errorf("expected multiply to fail")
@@ -135,102 +114,102 @@ func TestMultiply(t *testing.T) {
 	test_cases := []TestCase{
 		{
 			desc: "multiplication with the identity is the same",
-			input1: matrix{
+			input1: fromSliceOfSlices([][]float64{
 				{3.2, 3.0, 2.9},
 				{0.3, 1.23, 83.3},
 				{58.2, 12.1, 100},
-			},
-			input2: matrix{
+			}),
+			input2: fromSliceOfSlices([][]float64{
 				{1, 0, 0},
 				{0, 1, 0},
 				{0, 0, 1},
-			},
-			want: matrix{
+			}),
+			want: fromSliceOfSlices([][]float64{
 				{3.2, 3.0, 2.9},
 				{0.3, 1.23, 83.3},
 				{58.2, 12.1, 100},
-			},
+			}),
 		},
 		{
 			desc: "multiplication of two 2x2 matrix",
-			input1: matrix{
+			input1: fromSliceOfSlices([][]float64{
 				{2, 3},
 				{8, 1},
-			},
-			input2: matrix{
+			}),
+			input2: fromSliceOfSlices([][]float64{
 				{10, 4},
 				{11, 3},
-			},
-			want: matrix{
+			}),
+			want: fromSliceOfSlices([][]float64{
 				{53, 17},
 				{91, 35},
-			},
+			}),
 		},
 		{
 			desc: "multiplication of a wide 1x4 matrix by a tall 4x1 matrix",
-			input1: matrix{
+			input1: fromSliceOfSlices([][]float64{
 				{1.4, 2.4, 3.3, 9.1},
-			},
-			input2: matrix{
+			}),
+			input2: fromSliceOfSlices([][]float64{
 				{1.4},
 				{3.2},
 				{2.9},
 				{0.3},
-			},
-			want: matrix{
+			}),
+			want: fromSliceOfSlices([][]float64{
 				{21.94},
-			},
+			}),
 		},
 		{
 			desc: "multiplication by it's own inverse returns the identity",
-			input1: matrix{
+			input1: fromSliceOfSlices([][]float64{
 				{3, 4},
 				{1, 2},
-			},
-			input2: matrix{
+			}),
+			input2: fromSliceOfSlices([][]float64{
 				{1, -2},
 				{-0.5, 1.5},
-			},
-			want: matrix{
+			}),
+			want: fromSliceOfSlices([][]float64{
 				{1, 0},
 				{0, 1},
-			},
+			}),
 		},
 		{
 			desc: "multiplication by it's own inverse returns the identity (3x3)",
-			input1: matrix{
+			input1: fromSliceOfSlices([][]float64{
 				{1, 2, 3},
 				{1, 2, 1},
 				{1, 1, 4},
-			},
-			input2: matrix{
+			}),
+			input2: fromSliceOfSlices([][]float64{
 				{-3.5, 2.5, 2},
 				{1.5, -0.5, -1},
 				{0.5, -0.5, 0},
-			},
-			want: matrix{
+			}),
+			want: fromSliceOfSlices([][]float64{
 				{1, 0, 0},
 				{0, 1, 0},
 				{0, 0, 1},
-			},
+			}),
 		},
 		{
 			desc: "multiplication by it's own inverse returns the identity (fuzz)",
-			input1: matrix{
+			input1: fromSliceOfSlices([][]float64{
 				{1, 2, 3},
 				{3, 2, 1},
 				{2, 1, 3},
-			},
-			input2: matrix{
+			}),
+			input2: fromSliceOfSlices([][]float64{
 				{-5.0 / 12, 0.25, 1.0 / 3},
 				{7.0 / 12, 0.25, -2.0 / 3},
 				{1.0 / 12, -0.25, 1.0 / 3},
-			},
-			want: matrix{
+			}),
+			want: fromSliceOfSlices([][]float64{
 				{1, 0, 0},
 				{0, 1, 0},
 				{0, 0, 1},
-			},
+			}),
 		},
 	}
 
@@ -242,7 +221,7 @@ func TestMultiply(t *testing.T) {
 				t.Errorf("unexpected error %s", err)
 			}
 
-			if !reflect.DeepEqual(got, test_case.want) {
+			if !Equal(got, test_case.want) {
 				t.Errorf("expected to be the same, got %v, want %v", got, test_case.want)
 			}
 		})
@@ -257,11 +236,11 @@ func TestDet(t *testing.T) {
 	}
 
 	t.Run("fail on invalid multiplication (non-square matrix)", func(t *testing.T) {
-		m := matrix{
+		m := fromSliceOfSlices([][]float64{
 			{1},
 			{2},
 			{3},
-		}
+		})
 		_, err := Det(m)
 		if err == nil {
 			t.Errorf("expected determinant to fail")
@@ -271,19 +250,19 @@ func TestDet(t *testing.T) {
 	test_cases := []TestCase{
 		{
 			desc: "determinant of a 2x2 matrix",
-			input: matrix{
+			input: fromSliceOfSlices([][]float64{
 				{1, 2},
 				{3, 2},
-			},
+			}),
 			want: -4,
 		},
 		{
 			desc: "determinant of a 3x3 matrix",
-			input: matrix{
+			input: fromSliceOfSlices([][]float64{
 				{1, 2, 3},
 				{3, 2, 1},
 				{2, 1, 3},
-			},
+			}),
 			want: -12,
 		},
 	}
@@ -309,11 +288,11 @@ func TestInverse(t *testing.T) {
 	}
 
 	t.Run("fail on invalid multiplication (non-square matrix)", func(t *testing.T) {
-		m := matrix{
+		m := fromSliceOfSlices([][]float64{
 			{1},
 			{2},
 			{3},
-		}
+		})
 		_, err := Inverse(m)
 		if err == nil {
 			t.Errorf("expected inverse to fail")
@@ -323,27 +302,27 @@ func TestInverse(t *testing.T) {
 	test_cases := []TestCase{
 		{
 			desc: "inverse of the identity is the identity",
-			input: matrix{
+			input: fromSliceOfSlices([][]float64{
 				{1, 0, 0},
 				{0, 1, 0},
 				{0, 0, 1},
-			},
-			want: matrix{
+			}),
+			want: fromSliceOfSlices([][]float64{
 				{1, 0, 0},
 				{0, 1, 0},
 				{0, 0, 1},
-			},
+			}),
 		},
 		{
 			desc: "inverse of a 2x2 matrix",
-			input: matrix{
+			input: fromSliceOfSlices([][]float64{
 				{1, 2},
 				{3, 2},
-			},
-			want: matrix{
+			}),
+			want: fromSliceOfSlices([][]float64{
 				{0.5, -0.5},
 				{-0.75, 0.25},
-			},
+			}),
 		},
 	}
 
@@ -369,15 +348,15 @@ func TestAdd(t *testing.T) {
 	}
 
 	t.Run("fail on invalid addition (pxq + nxm)", func(t *testing.T) {
-		m1 := matrix{
+		m1 := fromSliceOfSlices([][]float64{
 			{2, 3},
 			{2, 3},
-		}
-		m2 := matrix{
+		})
+		m2 := fromSliceOfSlices([][]float64{
 			{1},
 			{2},
 			{3},
-		}
+		})
 		_, err := Add(m1, m2)
 		if err == nil {
 			t.Errorf("expected add to fail")
@@ -387,21 +366,21 @@ func TestAdd(t *testing.T) {
 	test_cases := []TestCase{
 		{
 			desc: "Add two matrices together",
-			input1: matrix{
+			input1: fromSliceOfSlices([][]float64{
 				{3.2, 3.0, 2.9},
 				{0.3, 1.23, 83.3},
 				{58.2, 12.1, 100},
-			},
-			input2: matrix{
+			}),
+			input2: fromSliceOfSlices([][]float64{
 				{1.2, 0.3, 0.1},
 				{2.0, 1.0, 7.0},
 				{8.3, 48.3, 100},
-			},
-			want: matrix{
+			}),
+			want: fromSliceOfSlices([][]float64{
 				{4.4, 3.3, 3.0},
 				{2.3, 2.23, 90.3},
 				{66.5, 60.4, 200},
-			},
+			}),
 		},
 	}
 
@@ -422,16 +401,16 @@ func TestAdd(t *testing.T) {
 
 func TestScale(t *testing.T) {
 	t.Run("scale by a factor", func(t *testing.T) {
-		x := matrix{
+		x := fromSliceOfSlices([][]float64{
 			{1, 2},
 			{3, 4},
-		}
+		})
 		a := 0.5
 		got := Scale(x, a)
-		want := matrix{
+		want := fromSliceOfSlices([][]float64{
 			{0.5, 1.0},
 			{1.5, 2.0},
-		}
+		})
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("expected to be the same, got %v, want %v", got, want)
 		}
