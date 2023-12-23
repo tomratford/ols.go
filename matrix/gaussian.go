7937e3f47df7631ef80f7dd74c77686148f230f4
Tom Ratford <tomratford@gmail.com>
2023-12-23 13:45:51 +0000

    feat(green): return a matrix in echelon form

--- matrix/gaussian.go
+++ matrix/gaussian.go
@@ -53,3 +53,65 @@ func AddToRow(x matrix, row1, row2 int, scale float64) (matrix, error) {
 
 	return Multiply(z,x)
 }
+
+// Return a matrix in echelon form
+func GaussianElimination(x matrix) (matrix, matrix, error) {
+	z := x.Copy()
+	p := Identity(x.N) // Permutation matrix
+
+	// Current Row
+	i := 0
+	// Current Column
+	j:= 0
+
+	for {
+		// Check to see if we actually need to do anything
+		if b, _ := IsUpperTriangular(z); b {
+			return z,p, nil
+		}
+		// If we are past x.M/x.N
+		if i >= x.N {
+			return z, p, nil
+		}
+		if i > x.M {
+			return z, p, nil
+		}
+
+		// Get pivot if unable to
+		if z.Get(i,j) == 0 {
+			for k:=i;k<=x.N;k++ {
+				if k == x.N {
+					j += 1 // All zero, move to next column
+					break
+				}
+				if z.Get(k,j) != 0 {
+					var err error
+					z, err = SwapRows(z, i, k)
+					if err != nil {
+						return matrix{},matrix{}, err
+					}
+					p, err = SwapRows(p, i, k) // We need to track permutations too
+					if err != nil {
+						return matrix{},matrix{}, err
+					}
+					break
+				}
+			}
+			continue // Restart whole loop with new i/j
+		}
+
+		// Make all other column entries zero
+		for k:=i+1;k<x.N;k++ {
+			var err error
+			scale := - (z.Get(k,j) / z.Get(i,j))
+			z, err = AddToRow(z, k, i, scale) // Add row scaled row i to row k
+			if err != nil {
+				return matrix{}, matrix{}, err
+			}
+		}
+
+		i += 1 // Move onto the next row to reduce
+
+		fuzzCheck(z) // Make any 'almost zeros' zero
+	}
+}
