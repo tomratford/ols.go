f8cd3a58b23a45ad99ddd759b3a85d8afc104b4e
Tom Ratford <tomratford@gmail.com>
2023-12-23 13:44:47 +0000

    feat(red): add a scaled row to a matrix

--- matrix/gaussian_test.go
+++ matrix/gaussian_test.go
@@ -86,3 +86,47 @@ func TestScaleRow(t *testing.T) {
 		})
 	}
 }
+
+func TestAddToRow(t *testing.T) {
+	type TestCase struct {
+		desc string
+		input, want matrix
+		row1, row2 int
+		scale float64
+	}
+
+	test_cases := []TestCase{
+		{
+			desc: "Swap rows in a matrix",
+			input: fromSliceOfSlices([][]float64{
+				{1.4, 4.4},
+				{3.2, 2.0},
+				{2.9, 9.3},
+				{0.3, 3.8},
+			}),
+			row1: 2,
+			row2: 0,
+			scale: -2,
+			want: fromSliceOfSlices([][]float64{
+				{1.4, 4.4},
+				{3.2, 2.0},
+				{0.1, 0.5},
+				{0.3, 3.8},
+			}),
+		},
+	}
+
+	for _, test_case := range test_cases {
+		t.Run(test_case.desc, func(t *testing.T) {
+			got, err := AddToRow(test_case.input, test_case.row1, test_case.row2, test_case.scale)
+
+			if err != nil {
+				t.Errorf("unexpected error: %s", err)
+			}
+
+			if !Equal(got, test_case.want) {
+				t.Errorf("expected to be the same, got %v, want %v", got, test_case.want)
+			}
+		})
+	}
+}
